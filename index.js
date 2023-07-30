da6a74d1609b3382c63daab105a3fcdf2a3c9345
devjohn58 <devjohn58@gmail.com>
2023-07-30 23:28:40 +0700

    fix 23.28

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 const { ethers } = require("ethers");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const ABIGAME = require("./abi")
 //============SERVER CONFIG=================
 
-const whitelist = ["http://localhost:5173", "https://react-horse-racing.vercel.app/"];
+const whitelist = [
+	"http://localhost:5173",
+	"https://react-horse-racing.vercel.app/",
+];
 const corsOptions = {
 	origin: function (origin, callback) {
 		if (whitelist.indexOf(origin) !== -1) {
@@ -18,24 +20,34 @@ const corsOptions = {
 	},
 };
 
-app.use(cors(corsOptions));
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"https://react-horse-racing.vercel.app/",
+		],
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // const provider = new ethers.JsonRpcProvider(
 //     "https://cloudflare-eth.com"
 const provider = new ethers.JsonRpcProvider(
-    "https://ethereum-goerli.publicnode.com"
+	"https://ethereum-goerli.publicnode.com"
 );
 
 //============FIREBASE CONFIG=================
 
 const { initializeApp } = require("firebase/app");
 const {
-    getFirestore,
+	getFirestore,
 	doc,
 	updateDoc,
-	increment,collection,query,where
+	increment,
+	collection,
+	query,
+	where,
 } = require("firebase/firestore");
 
 const firebaseConfig = {
@@ -55,101 +67,100 @@ const db = getFirestore(appFirebase);
 
 const deposit = async (id, amount) => {
 	try {
-        if (!id || !amount) return;
+		if (!id || !amount) return;
 		const userRef = doc(db, "users", id);
 		await updateDoc(userRef, {
 			balance: increment(amount),
 		});
 		return true;
 	} catch (error) {
-        return false;
+		return false;
 	}
 };
 
-
- const checkTxhash = async (txhash) => {
+const checkTxhash = async (txhash) => {
 	const collectionRef = query(
 		collection(db, "deposit"),
 		where("txhash", "==", txhash)
-    );
-    try {
-        const data = await getDocs(collectionRef);
-        if (data) {
-            return data?.docs?.map((doc) => ({ ...doc?.data(), id: doc?.id }));
-        }
-    } catch (error) {
-        return false;
-    }
+	);
+	try {
+		const data = await getDocs(collectionRef);
+		if (data) {
+			return data?.docs?.map((doc) => ({ ...doc?.data(), id: doc?.id }));
+		}
+	} catch (error) {
+		return false;
+	}
 };
 
 const confirmDeposit = async (id) => {
 	try {
-        if (!id) return;
+		if (!id) return;
 		const userRef = doc(db, "deposit", id);
 		await updateDoc(userRef, {
 			status: true,
 		});
 		return true;
 	} catch (error) {
-        return false;
+		return false;
 	}
 };
 
 app.use((req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (typeof authHeader !== 'undefined') {
-        // The header should look like "Bearer <token>". We only need the token part.
-        const token = authHeader.split(' ')[1];
-      req.token = token;
-    }
-    next();
+	const authHeader = req.headers["authorization"];
+	if (typeof authHeader !== "undefined") {
+		// The header should look like "Bearer <token>". We only need the token part.
+		const token = authHeader.split(" ")[1];
+		req.token = token;
+	}
+	next();
 });
 app.get("/", (req, res) => {
-    res.json({status: true})
-})
+	res.json({ status: true });
+});
 app.post("/api/getToken", async (req, res) => {
-    try {
-        if (!req.body) {
-            return res.status(403)
-        }
-        token = jwt.sign(req.body, process.env.JWT_SECRET);
-        res.json({token: token})
-    } catch (error) {
-        return res.status(403)
-    }
+	try {
+		if (!req.body) {
+			return res.status(403);
+		}
+		token = jwt.sign(req.body, process.env.JWT_SECRET);
+		res.json({ token: token });
+	} catch (error) {
+		return res.status(403);
+	}
 });
 
 app.post("/api/deposit", async (req, res) => {
-    if (!req.token) {
-        return res.status(403).json({error: "Not access!"})
-    }
-    try {
-        const data = jwt.verify(req.token, process.env.JWT_SECRET)
-        const { id, txhash, amount, idDeposit } = data
-        const _checkTxhash = await checkTxhash(txhash)
+	if (!req.token) {
+		return res.status(403).json({ error: "Not access!" });
+	}
+	try {
+		const data = jwt.verify(req.token, process.env.JWT_SECRET);
+		const { id, txhash, amount, idDeposit } = data;
+		const _checkTxhash = await checkTxhash(txhash);
 
-        if (_checkTxhash && _checkTxhash.length == 0) {
-            res.status(404)
-        }
-        if (!data.id || !data.txhash || !data.amount | !data.idDeposit) {
-            res.status(403)
-        }
-        const _tx = await provider.waitForTransaction(txhash);
-		if (_tx.status == 1) {
-            try {
-                await deposit(id, amount)
-                await confirmDeposit(idDeposit)
-                res.json({status: true})
-            } catch (error) {
-                return res.status(404);
-            }
-        } else {
-            return res.status(404);
+		if (_checkTxhash && _checkTxhash.length == 0) {
+			res.status(404);
 		}
-    } catch (error) {
-        console.log("Error: ", error);
-        res.json(403)
-    }
+		if (!data.id || !data.txhash || !data.amount | !data.idDeposit) {
+			res.status(403);
+		}
+		const _tx = await provider.waitForTransaction(txhash);
+		if (_tx.status == 1) {
+			try {
+				await deposit(id, amount);
+				await confirmDeposit(idDeposit);
+				res.json({ status: true });
+			} catch (error) {
+				return res.status(404);
+			}
+		} else {
+			return res.status(404);
+		}
+	} catch (error) {
+		console.log("Error: ", error);
+		res.json(403);
+	}
 });
 
 app.listen(3000);
